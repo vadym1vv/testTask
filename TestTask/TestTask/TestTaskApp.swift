abaf928d87e5970c11eac6b4721a8974a3b9a5ad
vadym01 <vasylaki95@gmail.com>
2025-05-16 16:34:29 +0300

    initial project state

---
+++ TestTask/TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Vadym Vasylaki on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
